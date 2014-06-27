a3ae8778b595f086b51356404c477e31759964d4
Florian Pirchner <florian.pirchner@gmail.com>
2014-06-27 16:03:18 +0200

    Migrated to xtext 2.6.1

--- org.lunifera.tools.maven.plugin.entity/src/main/java/org/lunifera/tooling/entity/ClassFileDebugSourceExtractor.java
+++ org.lunifera.tools.maven.plugin.entity/src/main/java/org/lunifera/tooling/entity/ClassFileDebugSourceExtractor.java
@@ -16,14 +16,18 @@ import java.io.File;
 import java.io.IOException;
 
 import org.objectweb.asm.ClassReader;
-import org.objectweb.asm.commons.EmptyVisitor;
+import org.objectweb.asm.ClassVisitor;
 
 import com.google.common.io.Files;
 
 public class ClassFileDebugSourceExtractor {
 
-	protected static class Visitor extends EmptyVisitor {
+	protected static class Visitor extends ClassVisitor {
 		protected String source;
+
+		public Visitor(int api) {
+			super(api);
+		}
 
 		@Override
 		public void visitSource(String arg0, String arg1) {
@@ -33,7 +37,7 @@ public class ClassFileDebugSourceExtractor {
 
 	public String getDebugSourceFileName(File classFile) throws IOException {
 		ClassReader cr = new ClassReader(Files.toByteArray(classFile));
-		Visitor visitor = new Visitor();
+		Visitor visitor = new Visitor(0);
 		cr.accept(visitor, ClassReader.SKIP_CODE | ClassReader.SKIP_FRAMES);
 		return visitor.source;
 	}
